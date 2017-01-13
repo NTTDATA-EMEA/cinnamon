d130fd7b2f0db6f14c34e39100e003b64c7d5ff4
davebassan <dave.bassan@magentys.co.uk>
2017-01-13 04:25:24 +0000

    Get cookies fix (#8)
    * check empty string, overload split
    
    * add unit test for options

---
+++ webdriver/src/test/java/io/magentys/cinnamon/webdriver/CinnamonWebDriverOptionsTest.java
@@ -0,0 +1,46 @@
+package io.magentys.cinnamon.webdriver;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.emptyCollectionOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+public class CinnamonWebDriverOptionsTest {
+
+    @Mock(extraInterfaces = { JavascriptExecutor.class, HasCapabilities.class })
+    private WebDriver mockDriver;
+
+    @Before
+    public void setUpMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldHandleEmptyCookiesForBrowsersThatRequireJavaScript() {
+        when(((HasCapabilities) mockDriver).getCapabilities()).thenReturn(DesiredCapabilities.safari());
+        doReturn("").when((JavascriptExecutor) mockDriver).executeScript("return (document.cookie);");
+        CinnamonWebDriverOptions options = new CinnamonWebDriverOptions(mockDriver);
+        assertThat(options.getCookies(), emptyCollectionOf(Cookie.class));
+    }
+
+    @Test
+    public void shouldHandleCookieWithEmptyValueForBrowsersThatRequireJavaScript() {
+        when(((HasCapabilities) mockDriver).getCapabilities()).thenReturn(DesiredCapabilities.safari());
+        doReturn("cookiea=;cookieb=123;cookiec=").when((JavascriptExecutor) mockDriver).executeScript("return (document.cookie);");
+        CinnamonWebDriverOptions options = new CinnamonWebDriverOptions(mockDriver);
+        assertThat(options.getCookieNamed("cookiea").getValue(), is(""));
+        assertThat(options.getCookieNamed("cookieb").getValue(), is("123"));
+    }
+
+}
