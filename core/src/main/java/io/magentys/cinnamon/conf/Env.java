881f10c5c0fa439880500928fcc54c5b79c9b5b9
abranyik <abranyik>
2017-06-07 10:47:22 +0100

    Revert "env.yml support added"
    This reverts commit 90d3905ba05a2905c7ace39846bb80305e8e65f2.

--- core/src/main/java/io/magentys/cinnamon/conf/Env.java
+++ core/src/main/java/io/magentys/cinnamon/conf/Env.java
@@ -7,10 +7,11 @@ import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -47,42 +48,16 @@ public class Env {
         Optional param = Optional.ofNullable(env);
         if (param.isPresent()) {
             this.env = env;
-            try {
-                config = initConfig();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            config = initConfig();
         } else {
             throw new RuntimeException("Cannot initialise Env. Please provide env profile parameter (-Denv=myProfile)");
         }
     }
 
-    private Config initConfig() throws IOException {
+    private Config initConfig() {
         Config systemConfig = ConfigFactory.systemProperties();
         File envConfig = searchConfigFileInClasspath(ConfigConstants.ENV_CONF_FILE);
-        if (envConfig.getName().contains(".yml")) {
-            return deleteFirstLine(envConfig);
-        }
-        if (envConfig.getName().contains(".conf")) {
-            return systemConfig.withFallback(ConfigFactory.parseFile(envConfig)).resolve().getConfig(env);
-        }
-        else {
-            throw new Error("The file [" + envConfig.getAbsolutePath() + "] is not an environment config file.");
-        }
-    }
-
-    private Config deleteFirstLine(File file) throws IOException {
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Yaml yaml = new Yaml();
-        Map<String, Map<String, Object>> data = ((Map<String, Map<String, Object>>) yaml
-                .load(inputStream));
-        Config systemConfig = ConfigFactory.systemProperties();
-        return systemConfig.withFallback(ConfigFactory.parseMap(data)).resolve().getConfig(env);
+        return systemConfig.withFallback(ConfigFactory.parseFile(envConfig)).resolve().getConfig(env);
     }
 
     private File searchConfigFileInClasspath(String filename) {
@@ -96,8 +71,7 @@ public class Env {
         if (files.size() == 0)
             throw new Error("Config file with name [" + filename + "] could not be found in your classpath.");
         if (files.size() > 1)
-//            log.warn("More than one file found for this environment with name [" + filename + "]");
-            throw new Error("More than one file found for this environment with name [" + filename + "]");
+            log.warn("More than one file found for this environment with name [" + filename + "]");
         if (!files.get(0).isFile())
             throw new Error("The file [" + files.get(0).getAbsolutePath() + "] is not a normal file.");
         return files.get(0);
