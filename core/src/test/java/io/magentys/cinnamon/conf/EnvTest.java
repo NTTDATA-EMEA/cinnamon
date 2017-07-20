469e851363d634028d9a5bb1782afbd490afde55
Dave Bassan <dave.bassan@magentys.co.uk>
2017-07-20 19:56:11 +0100

    Fix env test (#24)
    * Do not use absolute path to filter in Env. Running mvn release:perform copies source under target/checkout
    
    * bump acceptance-test version

--- core/src/test/java/io/magentys/cinnamon/conf/EnvTest.java
+++ core/src/test/java/io/magentys/cinnamon/conf/EnvTest.java
@@ -1,28 +1,39 @@
 package io.magentys.cinnamon.conf;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class EnvTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     private Env env;
 
-    //@Before
+    @Before
     public void setUp() {
         System.setProperty("env", "example");
         System.setProperty("example.host", "foobar");
         env = Env.INSTANCE;
     }
 
-    //@Test
-    public void testSystemPropertySubstitutionWorks() {
+    @Test
+    public void shouldGivePrecedenceToSystemProperty() {
         assertEquals("foobar", env.config.getString("host"));
     }
 
-    //@Test
-    public void testDefaultValueOfConfigItemWhenNoSystemPropertyOverrideIsSet() {
+    @Test
+    public void shouldUseConfigValueWhenNoSystemPropertyOverrideIsSet() {
         assertEquals(8080, env.config.getInt("port"));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNoEnvFileIsFound() {
+        thrown.expect(Error.class);
+        env.searchConfigFileInClasspath("nonexistent.conf");
     }
 }
