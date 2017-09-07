23cd7c85a1627b20b17335ee048cb39c95ff3609
Dave Bassan <dave.bassan@magentys.co.uk>
2017-09-07 11:03:10 +0100

    remove specific appium caps for ios and android

--- vendor-perfecto/src/main/scala/io/magentys/cinnamon/webdriver/remote/Perfecto.scala
+++ vendor-perfecto/src/main/scala/io/magentys/cinnamon/webdriver/remote/Perfecto.scala
@@ -11,28 +11,11 @@ class Perfecto extends CinnamonRemote {
 
   override def capabilities(browserProfile: String, config: Config): DesiredCapabilities = {
     val mainRemoteCaps: DesiredCapabilities = super.capabilities(browserProfile, config)
-    val additionalRemoteCaps = new DesiredCapabilities
-    val perfectoType = config.getString("capabilities-profiles." + browserProfile + ".perfecto.platformName")
-    if (perfectoType.equalsIgnoreCase("windows")) {
-    additionalRemoteCaps.setCapability("browserVersion", config.getString("capabilities-profiles." + browserProfile + ".perfecto.browserVersion"))
-    additionalRemoteCaps.setCapability("platformVersion", config.getString("capabilities-profiles." + browserProfile + ".perfecto.platformVersion"))
-    additionalRemoteCaps.setCapability("location", config.getString("capabilities-profiles." + browserProfile + ".perfecto.location"))
-    additionalRemoteCaps.setCapability("resolution", config.getString("capabilities-profiles." + browserProfile + ".perfecto.resolution"))
-    additionalRemoteCaps.setCapability("url", config.getString("hubUrl"))
-    additionalRemoteCaps.setCapability("platformName", config.getString("capabilities-profiles." + browserProfile + ".perfecto.platformName"))
-  }
 
-    else {
-      additionalRemoteCaps.setCapability("platformName", config.getString("capabilities-profiles." + browserProfile + ".perfecto.platformName"))
-      additionalRemoteCaps.setCapability("platformVersion", config.getString("capabilities-profiles." + browserProfile + ".perfecto.platformVersion"))
-      additionalRemoteCaps.setCapability("manufacturer", config.getString("capabilities-profiles." + browserProfile + ".perfecto.manufacturer"))
-      additionalRemoteCaps.setCapability("model", config.getString("capabilities-profiles." + browserProfile + ".perfecto.model"))
-      additionalRemoteCaps.setCapability("location", config.getString("capabilities-profiles." + browserProfile + ".perfecto.location"))
-      additionalRemoteCaps.setCapability("resolution", config.getString("capabilities-profiles." + browserProfile + ".perfecto.resolution"))
-      additionalRemoteCaps.setCapability("network", config.getString("capabilities-profiles." + browserProfile + ".perfecto.network"))
-      //browserName should not be send when running on iOS, this will become the appName in perfecto
-      additionalRemoteCaps.setCapability("browserName", "")
-    }
+    //TODO Here we can add names, build numbers etc - Dependency on the "handlers"
+    val additionalRemoteCaps = new DesiredCapabilities
+    //    additionalRemoteCaps.setCapability("name", "someName")
+    //    additionalRemoteCaps.setCapability("build", "someBuild")
 
     mainRemoteCaps.merge(additionalRemoteCaps)
   }
