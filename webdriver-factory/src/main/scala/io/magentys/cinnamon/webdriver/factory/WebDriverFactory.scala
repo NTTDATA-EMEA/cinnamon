48b0ee124589e689b2e864b659e92f162063b14e
Attila Branyik <attilabranyik@Administrators-MacBook-Pro.local>
2018-04-17 15:37:19 +0100

    REmoteWEbDriverFactory removed

--- webdriver-factory/src/main/scala/io/magentys/cinnamon/webdriver/factory/WebDriverFactory.scala
+++ webdriver-factory/src/main/scala/io/magentys/cinnamon/webdriver/factory/WebDriverFactory.scala
@@ -1,10 +1,13 @@
 package io.magentys.cinnamon.webdriver.factory
 
 import java.nio.file.{Files, Paths}
+import java.net.URL
 
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.ios.IOSDriver
 import io.github.bonigarcia.wdm.{BrowserManager, WebDriverManager}
 import io.magentys.cinnamon.webdriver.capabilities.DriverBinary
-import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
 import org.openqa.selenium.{Capabilities, WebDriver}
 
 import scala.util.Try
@@ -28,7 +31,7 @@ class WebDriverFactory(factory: WebDriverManagerFactory) {
   def getDriver(capabilities: DesiredCapabilities, hubUrl: Option[String], exePath: Option[String], driverBinary: Option[DriverBinary]): WebDriver = {
 
     if (hubUrl.isDefined && !hubUrl.get.isEmpty) {
-      return RemoteWebDriverFactory.getRemoteDriver(capabilities, hubUrl)
+      return getRemoteDriver(capabilities, hubUrl)
     }
 
     val driverClass = DriverRegistry.getDriverClass(capabilities) match {
@@ -47,6 +50,15 @@ class WebDriverFactory(factory: WebDriverManagerFactory) {
     }
     driverClass.getDeclaredConstructor(classOf[Capabilities]).newInstance(capabilities)
   }
+
+  def getRemoteDriver(capabilities: DesiredCapabilities, hubUrl: Option[String]): RemoteWebDriver = {
+    capabilities.getCapability("platformName") match {
+      case "Android" => new AndroidDriver(new URL(hubUrl.get), capabilities)
+      case "iOS" => new IOSDriver(new URL(hubUrl.get), capabilities)
+      case _ => new RemoteWebDriver(new URL(hubUrl.get), capabilities)
+    }
+  }
+
 }
 
 object WebDriverFactory {
