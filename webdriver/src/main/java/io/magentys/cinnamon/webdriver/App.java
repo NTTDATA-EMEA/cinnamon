aedad81c336ffd9ac4fabb1ce69ccdc3f9bc4e7e
Attila Branyik <attilabranyik@Administrators-MacBook-Pro.local>
2018-04-27 09:50:21 +0100

    apiumdriver added to App.java

---
+++ webdriver/src/main/java/io/magentys/cinnamon/webdriver/App.java
@@ -0,0 +1,34 @@
+package io.magentys.cinnamon.webdriver;
+
+import io.appium.java_client.AppiumDriver;
+
+public final class App {
+
+    /**
+     * Appium function - will close the running app
+     */
+    public static void closeApp() {
+        AppiumDriver driver = getAppiumDriver();
+        driver.closeApp();
+    }
+
+    /**
+     * Appium function - will start the app provided in the webdriver conf
+     */
+    public static void startApp() {
+        AppiumDriver driver = getAppiumDriver();
+        driver.launchApp();
+    }
+
+    /**
+     * Appium function - will rest and restart the running app
+     */
+    public static void resetApp() {
+        AppiumDriver driver = getAppiumDriver();
+        driver.resetApp();
+    }
+
+    private static AppiumDriver getAppiumDriver() {
+        return (AppiumDriver) Browser.getWebDriver();
+    }
+}
