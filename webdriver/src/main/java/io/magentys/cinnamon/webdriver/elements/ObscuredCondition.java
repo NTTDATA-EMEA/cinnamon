3d5526c00acb6b86d4af51f1e98f38d80bb4f4b4
Attila Branyik <attilabranyik@Administrators-MacBook-Pro.local>
2018-07-02 09:37:42 +0100

    commented line deleted

--- webdriver/src/main/java/io/magentys/cinnamon/webdriver/elements/ObscuredCondition.java
+++ webdriver/src/main/java/io/magentys/cinnamon/webdriver/elements/ObscuredCondition.java
@@ -17,7 +17,6 @@ public class ObscuredCondition extends Condition<WebElement> {
             return (Boolean) js.executeScript(
                     "var element = document.elementFromPoint(arguments[1], arguments[2]);" + "return element.parentNode !== arguments[0].parentNode",
                     element, element.getLocation().x + element.getSize().width / 2, element.getLocation().y + element.getSize().height / 2);
-//                    element, element.getLocation().x, element.getLocation().y);
         } catch (NullPointerException | WebDriverException e) {
             return false;
         }
