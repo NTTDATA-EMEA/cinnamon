3c928ca05675746348c6a276ad8d2030cd988f2d
Dan <dan.watkinson@gmail.com>
2019-01-11 11:16:37 +0000

    updated cucumber dependencies; fix first set of resulting compilation issues

--- cucumber/src/main/java/io/magentys/cinnamon/cucumber/events/StepFinishedEvent.java
+++ cucumber/src/main/java/io/magentys/cinnamon/cucumber/events/StepFinishedEvent.java
@@ -1,17 +1,21 @@
 package io.magentys.cinnamon.cucumber.events;
 
-import gherkin.formatter.Reporter;
-import gherkin.formatter.model.Result;
+import cucumber.api.event.EmbedEvent;
+import cucumber.api.event.Event;
+import cucumber.api.event.EventListener;
+import cucumber.api.Result;
 import io.magentys.cinnamon.events.Attachment;
 import io.magentys.cinnamon.events.TestStepFinishedEvent;
 
+import java.util.Date;
+
 public class StepFinishedEvent implements TestStepFinishedEvent {
     private final Result result;
-    private final Reporter reporter;
+    private final EventListener eventListener;
 
-    public StepFinishedEvent(final Result result, final Reporter reporter) {
+    public StepFinishedEvent(final Result result, final EventListener eventListener) {
         this.result = result;
-        this.reporter = reporter;
+        this.eventListener = eventListener;
     }
 
     @Override
@@ -20,12 +24,15 @@ public class StepFinishedEvent implements TestStepFinishedEvent {
     }
 
     @Override
-    public String getStatus() {
+    public Result.Type getStatus() {
         return result.getStatus();
     }
 
     @Override
     public void attach(Attachment attachment) {
-        reporter.embedding(attachment.getMimeType(), attachment.getBytes());
+        //TODO should i be creating my own timestamp here, or does it need to be passed in somehow/somewhere
+        Long timestamp = new Date().getTime();
+        //TODO provide TestCase here...
+        Event event =  new EmbedEvent(timestamp, null, attachment.getBytes(), attachment.getMimeType());
     }
 }
