5d16276cb2db0489be736949e52373f8adfe4760
Dave Bassan <dave.bassan@magentys.io>
2019-04-01 17:00:08 +0100

    bump guice version to align with cucumber update guice modules to reflect new APIs remove cucumber-jvm-parallel-plugin

--- acceptance-test/src/test/java/com/acme/samples/acceptancetests/AllTests.java
+++ acceptance-test/src/test/java/com/acme/samples/acceptancetests/AllTests.java
@@ -1,12 +1,12 @@
 package com.acme.samples.acceptancetests;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue = { "io.jmcore", "com.acme" }, features = "src/test/resources/features", plugin = {"pretty",
-        "json:target/cucumber-reports/cucumber.json"}, tags = { "@complete,@accepted" })
+@CucumberOptions(extraGlue = { "com.acme.samples.local.stepdef", "com.acme.samples.local.context", "com.acme.samples.google.stepdef",
+        "com.acme.samples.google.context" }, features = "src/test/resources/features", plugin = { "progress",
+        "json:target/cucumber-reports/cucumber.json" }, tags = { "@complete,@accepted" })
 public class AllTests {
 }
