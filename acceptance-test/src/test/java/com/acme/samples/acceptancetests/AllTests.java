3645428a87bbb3dcf951bf039f776335709c3e85
Dave Bassan <dave.bassan@magentys.io>
2019-04-03 15:34:16 +0100

    cucumber no longer supports List<String> as stepdef arg for single non-delimited string

--- acceptance-test/src/test/java/com/acme/samples/acceptancetests/AllTests.java
+++ acceptance-test/src/test/java/com/acme/samples/acceptancetests/AllTests.java
@@ -5,8 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(extraGlue = { "com.acme.samples.local.stepdef", "com.acme.samples.local.context", "com.acme.samples.google.stepdef",
-        "com.acme.samples.google.context" }, features = "src/test/resources/features", plugin = { "progress",
-        "json:target/cucumber-reports/cucumber.json" }, tags = { "@complete,@accepted" })
+@CucumberOptions(glue = { "com.acme.samples.local.stepdef", "com.acme.samples.google.stepdef" }, features = "src/test/resources/features", plugin = {
+        "progress", "json:target/cucumber-reports/cucumber.json" }, tags = { "@complete,@accepted" })
 public class AllTests {
 }
