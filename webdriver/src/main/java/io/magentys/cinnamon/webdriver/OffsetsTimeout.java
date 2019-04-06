b241a43aa2adf94f8a79658159e9d03947d83479
Dave Bassan <dave.bassan@magentys.io>
2019-04-06 07:54:01 +0100

    deprecate TimeUnit but retain backwards compatibility in interim

--- webdriver/src/main/java/io/magentys/cinnamon/webdriver/OffsetsTimeout.java
+++ webdriver/src/main/java/io/magentys/cinnamon/webdriver/OffsetsTimeout.java
@@ -1,15 +1,22 @@
 package io.magentys.cinnamon.webdriver;
 
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 public interface OffsetsTimeout extends Timeout {
 
+    Timeout plus(long time, ChronoUnit unit);
+
+    @Deprecated
     Timeout plus(long time, TimeUnit unit);
 
     Timeout plusMillis(long millis);
 
     Timeout plusSeconds(long seconds);
 
+    Timeout minus(long time, ChronoUnit unit);
+
+    @Deprecated
     Timeout minus(long time, TimeUnit unit);
 
     Timeout minusMillis(long millis);
